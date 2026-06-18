d97f45b67c7eae1d6844ef6c2ddb4a2b0f24b6f3
TaiMeade <taitaishi123@gmail.com>
2026-06-18 16:57:24 -0400

    added review from Google

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -14,4 +14,10 @@ export const reviews = [
     text: "Thank you Legacy Pool Co. Max Ziemer and Aubrianna Ziemer were on it! I cannot thank them enough for the very quick and efficient, and honest service. Turns out it was a fairly inexpensive fix, thank goodness!!! Anyone in need of pool service, reach out to them. I will for sure be using them again.",
     date: '2026-06-11',
   },
+  {
+    name: 'Kirsten Tuggle',
+    rating: 5,
+    text: "I wish I could leave 10 stars!!! ⭐️ ⭐️ ⭐️!!! Best pool company in town in my opinion. Very knowledgeable and consistent!! My pool has never looked better. Also, The water doesn’t leave my skin and hair feeling gross- they use the best chemicals and my eyes don’t burn from too much chlorine. The best part is, you are a supporting a local family business when you use them. When you need a repair, they will fix it and not charge an arm and a leg to do so. I can’t recommend them enough. Very satisfied and will be telling all of my family and friends to use them! Thank you Legacy Pool Co.",
+    date: '2026-06-17',
+  }
 ]
